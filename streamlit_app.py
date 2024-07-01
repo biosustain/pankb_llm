f0be00b65f0520f415c6388d04f3430963282eb1
Binhuan Sun <binsun@dtu.dk>
2024-07-01 11:59:40 +0000

    Change on the streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ from operator import itemgetter
 from langchain.schema.runnable import RunnableMap
 from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
 from langchain_cohere import CohereRerank
-from langchain_community.llms import Cohere
 from langchain_community.vectorstores.azure_cosmos_db import AzureCosmosDBVectorSearch, CosmosDBSimilarityType, CosmosDBVectorSearchType
 from langchain_voyageai import VoyageAIEmbeddings
 import streamlit as st
@@ -40,10 +39,10 @@ collection = client[db_name][collection_name]
 
 
 def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+        return "\n\n".join('Title: ' + doc.metadata['title'] + '.' +' Content: ' + doc.page_content for doc in docs)
 
 def filter_and_extract_documents(documents):
-    filtered_documents = [doc for doc in documents if doc.metadata['relevance_score'] >= 0.7]
+    filtered_documents = [doc for doc in documents if doc.metadata['relevance_score'] >= 0.5]
     return filtered_documents
 
 def get_retriever(db_name, collection_name):
@@ -56,7 +55,6 @@ def get_retriever(db_name, collection_name):
 
     retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 30})
 
-    llm = Cohere(temperature=0)
     compressor = CohereRerank(top_n=20)
     compression_retriever = ContextualCompressionRetriever(
         base_compressor=compressor, base_retriever=retriever
@@ -74,7 +72,7 @@ def question_answer(retriever, question):
 
     prompt = ChatPromptTemplate.from_template(template)
 
-    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature=0)
 
     rag_chain_from_docs = (
             {
@@ -111,7 +109,7 @@ def question_answer(retriever, question):
 
 
 if __name__ == "__main__":
-    st.title("PangenomeLLM: Your Microbial Pangenomics Assistant")
+    st.title("PanKB LLM: Your Microbial Pangenomics Assistant")
 
     # Initialize your retriever just once
     retriever = get_retriever(db_name, collection_name)
